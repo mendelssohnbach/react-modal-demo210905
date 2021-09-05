468c31aca38a2d39b7618329a54c95f11f5fceb1
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-09-05 11:32:07 +0900

    :sparkles: AppコンポーネントでModalコンポーネント制御を行う
    https://gitmoji.dev/

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 import Modal from './components/Modal';
 
 function App() {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
     <div className="App">
       <h1>ボタンをクリックしてモーダルを開きます。</h1>
-      <button className="openModalBtn">Open</button>
-      <Modal />
+      <button className="openModalBtn" onClick={() => setOpenModal(true)}>
+        Open
+      </button>
+      {openModal && <Modal />}
     </div>
   );
 }
