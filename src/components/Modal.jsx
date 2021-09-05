59543cdb1facd1b984deca431ce597fcedeeae6a
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-09-05 11:23:17 +0900

    :sparkles: Modalコンポーネント簡易な構造作成
    https://gitmoji.dev/

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,21 @@
 const Modal = () => {
-  return <div>Modal page.</div>;
+  return (
+    <div className="modalBackground">
+      <div className="modalContainer">
+        <button> X </button>
+        <div className="title">
+          <h1>続行してもよろしいですか？</h1>
+        </div>
+        <div className="body">
+          <p>次のページがすごいことになっています。行ってみたくなりましたか？</p>
+        </div>
+        <div className="footer">
+          <button>中止</button>
+          <button>続ける</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
